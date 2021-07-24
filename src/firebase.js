904054c522a51ed6f9a1777d9a8fdbf755233c4b
ldh3907 <ldh165163@gmail.com>
2021-07-24 22:37:18 +0900

    setting : react files
    setting : react files

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5bDFzqfeMqbXDjDiErpbRy6i6E-415Bc",
+  authDomain: "twitter-clone-4561b.firebaseapp.com",
+  projectId: "twitter-clone-4561b",
+  storageBucket: "twitter-clone-4561b.appspot.com",
+  messagingSenderId: "326046460264",
+  appId: "1:326046460264:web:d2b9be2ec836fb82608edd",
+};
+
+export default firebase.initializeApp(firebaseConfig);
