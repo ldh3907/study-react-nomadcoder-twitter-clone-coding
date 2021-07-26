8f280f808592ef9dfa6616ad6c4dcc535823474b
ldh3907 <ldh165163@gmail.com>
2021-07-26 17:57:12 +0900

    update : home
    트윗 폼, 파이어베이스 데이터베이스 연동

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,30 @@
-const Home = () => <span>Home</span>;
+import { useState } from "react";
+
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="무슨 생각을 하고 계신가요?"
+          maxLength={120}
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="게시" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
