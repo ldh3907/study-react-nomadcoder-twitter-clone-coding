8174493676ed615d484a5be4737521c05ea0c49e
ldh3907 <ldh165163@gmail.com>
2021-07-26 17:00:46 +0900

    update : Auth
    구글계정 로그인, 깃허브 계정 로그인

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "MyBase";
+import { authService, firebaseInstance } from "MyBase";
 import { useState } from "react";
 
 const Auth = () => {
@@ -36,8 +36,18 @@ const Auth = () => {
   };
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
-  const onSocialClick = (event) => {
-    console.log(event.target.name);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
   return (
     <div>
@@ -65,10 +75,10 @@ const Auth = () => {
         {newAccount ? "로그인" : "계정 만들기"}
       </span>
       <div>
-        <button onclick={onSocialClick} name="google">
+        <button onClick={onSocialClick} name="google">
           구글계정으로 로그인하기
         </button>
-        <button onclick={onSocialClick} name="github">
+        <button onClick={onSocialClick} name="github">
           깃허브 계정으로 로그인하기
         </button>
       </div>
