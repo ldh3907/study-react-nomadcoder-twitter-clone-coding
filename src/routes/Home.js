7f67b934da35becd68d5406ce7b375666f1d0f42
ldh3907 <ldh165163@gmail.com>
2021-07-27 21:55:51 +0900

    update: Home
    파이어베이스의 파이어스토어에 있는 트윗들을 가져옴

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
 import { dbService } from "MyBase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("tweets").add({
@@ -17,6 +33,7 @@ const Home = () => {
     } = event;
     setTweet(value);
   };
+  console.log(tweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -29,6 +46,13 @@ const Home = () => {
         />
         <input type="submit" value="게시" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
