983d7d8348bef15724932d63643e9bf357669a09
ldh3907 <ldh165163@gmail.com>
2021-07-29 23:56:56 +0900

    update : App, Home
    사진 미리보기, 미리보기 삭제 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,6 @@ function App() {
       ) : (
         "Initializing..."
       )}
-      <footer>&copy; Twitter Clone {new Date().getFullYear()}</footer>
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection("tweets").onSnapshot((snapshot) => {
@@ -30,6 +31,21 @@ const Home = ({ userObj }) => {
     } = event;
     setTweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -40,7 +56,14 @@ const Home = ({ userObj }) => {
           value={tweet}
           onChange={onChange}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="게시" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>삭제</button>
+          </div>
+        )}
       </form>
       <div>
         {tweets.map((tweet) => (
