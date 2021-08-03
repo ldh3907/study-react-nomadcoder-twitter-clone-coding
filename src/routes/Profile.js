4cac39abb6dd2ba3a8a030c3d89ea28857006ee9
ldh3907 <ldh165163@gmail.com>
2021-08-03 19:45:56 +0900

    update : profile
    프로필 부분 css 작업 완료

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,12 @@
 import { authService, dbService } from "MyBase";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import "./Profile.css";
+import Edit from "img/Edit.svg";
 
 export default ({ userObj, refreshUser }) => {
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const [isClick, setIsClick] = useState(false);
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
@@ -35,21 +38,39 @@ export default ({ userObj, refreshUser }) => {
         displayName: newDisplayName,
       });
       refreshUser();
+      setIsClick(false);
     }
   };
+
+  const toggleRename = () => setIsClick((prev) => !prev);
+
   return (
     <>
       <section id="profile">
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            onChange={onChange}
-            placeholder="변경하실 이름을 적어주세요"
-            value={newDisplayName}
-          />
-          <input type="submit" value="이름 변경" />
-        </form>
-        <button onClick={onLogOutClick}>로그아웃</button>
+        <div id="profile-form">
+          <form id="profile-rename-wrap" onSubmit={onSubmit}>
+            <div id="profile-top-wrap">
+              <h1>{userObj.displayName}</h1>
+              <button id="tweet-rename-btn" onClick={toggleRename}>
+                <img src={Edit} />
+              </button>
+            </div>
+            {isClick ? (
+              <>
+                <input
+                  type="text"
+                  onChange={onChange}
+                  placeholder="변경하실 이름을 적어주세요"
+                  value={newDisplayName}
+                />
+                <input type="submit" value="이름 변경" />
+              </>
+            ) : null}
+          </form>
+          <button id="profile-logOut-btn" onClick={onLogOutClick}>
+            로그아웃
+          </button>
+        </div>
       </section>
     </>
   );
